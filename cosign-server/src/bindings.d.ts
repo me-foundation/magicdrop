5aadbe69c3ee8bbe8556ab68ecac4bf71a2b89bc
Rex <rex@magiceden.io>
2022-09-25 22:53:44 -0700

    Add cosign server

---
+++ cosign-server/src/bindings.d.ts
@@ -0,0 +1,13 @@
+/**
+ * ENV variable
+ */
+
+declare const COSIGN_PRIVATE_KEY: string;
+declare const DATADOG_SK: string;
+declare const ADMIN_KEY: string;
+
+/**
+ * COLLECTIONS -> CloudFlare binding to KV Namespace
+ */
+
+declare const COLLECTIONS: KVNamespace;
---
+++ cosign-server/src/datadog.ts
@@ -0,0 +1,32 @@
+const ddUrl = "https://http-intake.logs.datadoghq.com/api/v2/logs";
+
+type LogStatus = "success" | "error" | "warn" | "info";
+
+export function dd(
+    event: FetchEvent,
+    data: Record<string, any>,
+    status?: LogStatus
+) {
+    if (DATADOG_SK.length === 0) {
+        return;
+    }
+
+    const hostname = event.request.headers.get("host") || "";
+    event.waitUntil(
+        fetch(ddUrl, {
+            method: "POST",
+            body: JSON.stringify({
+                ddsource: "cloudflare",
+                ddtags: "site:" + hostname,
+                hostname: hostname,
+                service: "erc721m-cosign-server",
+                message: JSON.stringify(data),
+                status: status ?? "success",
+            }),
+            headers: new Headers({
+                "Content-Type": "application/json",
+                "DD-API-KEY": DATADOG_SK,
+            }),
+        })
+    );
+}
