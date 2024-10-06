808657984b5cb521b340044c657a1d67b4f7adb7
Wolfy <wolfynft@gmail.com>
2024-10-06 12:04:24 -0400

    cleanup
    Signed-off-by: Wolfy <wolfynft@gmail.com>

--- contracts/common/interfaces/IInitializableToken.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface IInitializableToken {
-    function initialize(string calldata name, string calldata symbol, address payable initialOwner) external;
-}
