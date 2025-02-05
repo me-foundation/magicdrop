41b5215778a2f3b790c39014930e8204e0f879ae
Tom Hirst <hello@tomhirst.com>
2025-02-05 21:12:52 +0000

    add mintFee to struct

--- contracts/common/Structs.sol
+++ contracts/common/Structs.sol
@@ -47,4 +47,6 @@ struct SetupConfig {
     address royaltyRecipient;
     /// @dev The royalty basis points of the token.
     uint96 royaltyBps;
+    /// @dev The mint fee of the token.
+    uint256 mintFee;
 }
