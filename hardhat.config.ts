c4c50b85ef82d6c06bb7b7dd8e35dff0edacde3f
justinwalsh-me <justin.walsh@magiceden.io>
2025-02-17 22:38:18 -0500

    feat: add monad-testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -133,6 +133,11 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    'monad-testnet': {
+      url: process.env.MONAD_TESTNET_URL || 'https://testnet-rpc2.monad.xyz/f8146cc42d8671ba15204cff0e3608faf370d983',
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    }
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
