0314a2258b947741e59477b99d9690f89c21ec41
Tom Hirst <hello@tomhirst.com>
2025-03-03 11:59:47 +0000

    fix totalMintedByStage on 1155 v1_0

--- contracts/nft/erc1155m/ERC1155M.sol
+++ contracts/nft/erc1155m/ERC1155M.sol
@@ -138,7 +138,7 @@ contract ERC1155M is
             revert InvalidStage();
         }
         uint256[] memory walletMinted = totalMintedByAddress(msg.sender);
-        uint256[] memory stageMinted = _totalMintedByStageByAddress(stage, msg.sender);
+        uint256[] memory stageMinted = _totalMintedByStage(stage);
         return (_mintStages[stage], walletMinted, stageMinted);
     }
 
@@ -439,19 +439,13 @@ contract ERC1155M is
         return totalMinted;
     }
 
-    /// @dev Calculates the total minted tokens for a specific address in a given stage
+    /// @dev Calculates the total minted tokens for a given stage
     /// @param stage The stage number
-    /// @param account The address to check
     /// @return An array of total minted tokens for each token ID in the given stage
-    function _totalMintedByStageByAddress(uint256 stage, address account)
-        internal
-        view
-        virtual
-        returns (uint256[] memory)
-    {
+    function _totalMintedByStage(uint256 stage) internal view virtual returns (uint256[] memory) {
         uint256[] memory totalMinted = new uint256[](_numTokens);
         for (uint256 token = 0; token < _numTokens; token++) {
-            totalMinted[token] += _stageMintedCountsPerTokenPerWallet[stage][token][account];
+            totalMinted[token] += _stageMintedCountsPerToken[stage][token];
         }
         return totalMinted;
     }
