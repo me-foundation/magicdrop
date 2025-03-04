bc9b781ff16a99cc68d1bb7cbbc0418d90fc1921
Tom Hirst <hello@tomhirst.com>
2025-03-04 13:10:01 +0000

    update deploy/register impl scripts

--- scripts-foundry/common/2a-deploy-magicdrop-impl.sh
+++ scripts-foundry/common/2a-deploy-magicdrop-impl.sh
@@ -21,11 +21,13 @@ RPC_URL=""
 STANDARD=""
 IMPL_EXPECTED_ADDRESS=""
 IMPL_SALT=""
+USE_CASE=""
+IS_ERC721C=false
 
 # Function to display usage
 usage() {
-    # Example Usage: ./2a-deploy-magicdrop-impl.sh --chain-id 137 --token-standard ERC721 --expected-address 0x0000000000000000000000000000000000000000 --salt 0x0000000000000000000000000000000000000000000000000000000000000000
-    echo "Usage: $0 --chain-id <chain id> --version <magic drop impl version> --token-standard <token standard> --expected-address <expected address> --salt <salt>"
+    # Example Usage: ./2a-deploy-magicdrop-impl.sh --chain-id 137 --token-standard ERC721 --use-case launchpad --is-erc721c true --expected-address 0x0000000000000000000000000000000000000000 --salt 0x0000000000000000000000000000000000000000000000000000000000000000
+    echo "Usage: $0 --chain-id <chain id> --token-standard <token standard> --use-case <launchpad | self-serve> --is-erc721c <bool (optional)> --expected-address <expected address> --salt <salt>"
     exit 1
 }
 
@@ -34,15 +36,15 @@ while [[ "$#" -gt 0 ]]; do
     case $1 in
         --chain-id) CHAIN_ID=$2; shift ;;
         --token-standard) STANDARD=$2; shift ;;
+        --use-case) USE_CASE=$2; shift ;;
         --expected-address) IMPL_EXPECTED_ADDRESS=$2; shift ;;
         --salt) IMPL_SALT=$2; shift ;;
-        *) usage ;;
     esac
     shift
 done
 
 # Check if all parameters are set
-if [ -z "$CHAIN_ID" ] || [ -z "$STANDARD" ] || [ -z "$IMPL_EXPECTED_ADDRESS" ] || [ -z "$IMPL_SALT" ]; then
+if [ -z "$CHAIN_ID" ] || [ -z "$STANDARD" ] || [ -z "$USE_CASE" ] || [ -z "$IMPL_EXPECTED_ADDRESS" ] || [ -z "$IMPL_SALT" ]; then
     usage
 fi
 
@@ -60,6 +62,8 @@ echo "==================== DEPLOYMENT DETAILS ===================="
 echo "Chain ID:                     $CHAIN_ID"
 echo "RPC URL:                      $RPC_URL"
 echo "Token Standard:               $STANDARD"
+echo "isERC721C:                    $IS_ERC721C"
+echo "Use Case:                     $USE_CASE"
 echo "Expected Address:             $IMPL_EXPECTED_ADDRESS"
 echo "Salt:                         $IMPL_SALT"
 echo "============================================================"
@@ -79,7 +83,7 @@ echo "============= DEPLOYING MAGICDROP IMPLEMENTATION ============="
 echo ""
 
 # remove --verify when deploying on Sei Chain. You will need to verify manually.
-CHAIN_ID=$CHAIN_ID RPC_URL=$RPC_URL TOKEN_STANDARD=$STANDARD IMPL_EXPECTED_ADDRESS=$IMPL_EXPECTED_ADDRESS IMPL_SALT=$IMPL_SALT forge script ./DeployMagicDropImplementation.s.sol:DeployMagicDropImplementation \
+CHAIN_ID=$CHAIN_ID RPC_URL=$RPC_URL TOKEN_STANDARD=$STANDARD IMPL_EXPECTED_ADDRESS=$IMPL_EXPECTED_ADDRESS IMPL_SALT=$IMPL_SALT IS_ERC721C=$IS_ERC721C USE_CASE=$USE_CASE forge script ./DeployMagicDropImplementation.s.sol:DeployMagicDropImplementation \
   --rpc-url $RPC_URL \
   --broadcast \
   --optimizer-runs 777 \
