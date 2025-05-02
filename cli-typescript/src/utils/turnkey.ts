d6f8d3b776b7441994404a670211b5ce62df94d9
Ogooluwa Akinola <ogooluwa.akinola@magiceden.io>
2025-05-02 09:16:32 -0400

    chore: Turnkey Security - move turnkey env to aws - implement turnkey setup and signer setup

--- cli-typescript/src/utils/turnkey.ts
+++ cli-typescript/src/utils/turnkey.ts
@@ -1,5 +1,3 @@
-import { ApiKeyStamper } from '@turnkey/api-key-stamper';
-import { TurnkeyClient } from '@turnkey/http';
 import { createAccount } from '@turnkey/viem';
 import { Account, Hex } from 'viem';
 import {
@@ -7,92 +5,108 @@ import {
   Turnkey as TurnkeySDKServer,
 } from '@turnkey/sdk-server';
 import { getWalletStore } from './fileUtils';
+import { fetchSecretFromAWS } from './common';
+import { showText } from './display';
 
-export const getTurnkey = async (): Promise<{
-  turnkey: TurnkeyClient;
-  account: Account;
-}> => {
-  let turnkeyInstance: TurnkeyClient | null = null;
-  let turnkeyAccount: Account | null = null;
+const TURNKEY_SECRET_NAME =
+  process.env.ENVIRONMENT === 'PROD'
+    ? 'eng-magicdrop-prod'
+    : 'eng-magicdrop-dev';
+const TURNKEY_BASE_URL = 'https://api.turnkey.com';
+const ETHEREUM_WALLET_DEFAULT_PATH = "m/44'/60'/0'/0/0"; // eslint-disable-line quotes
+let turnkeySecrets: Record<string, string> | null = null;
 
-  // Validate required environment variables
-  validateTurnkeyEnvironmentVariables(['TURNKEY_WALLET_ADDRESS']);
-
-  if (!turnkeyInstance) {
-    turnkeyInstance = new TurnkeyClient(
-      {
-        baseUrl: 'https://api.turnkey.com',
-      },
-
-      new ApiKeyStamper({
-        apiPublicKey: process.env.TURNKEY_API_PUBLIC_KEY!,
-        apiPrivateKey: process.env.TURNKEY_API_PRIVATE_KEY!,
-      }),
-    );
-  }
-
-  if (!turnkeyAccount) {
-    turnkeyAccount = await createAccount({
-      client: turnkeyInstance,
-      organizationId: process.env.TURNKEY_ORG_ID!,
-      signWith: process.env.TURNKEY_WALLET_ADDRESS!,
-    });
-  }
-
-  return { turnkey: turnkeyInstance, account: turnkeyAccount };
-};
-
-// Helper function to validate required environment variables
-const validateTurnkeyEnvironmentVariables = (
+/**
+ * Fetches and validates Turnkey environment variables from AWS Secrets Manager.
+ * @param secretName The name of the AWS Secrets Manager secret.
+ * @param otherEnvs Additional environment variables to validate.
+ */
+const validateTurnkeyEnvironmentVariables = async (
+  secretName: string,
   otherEnvs: string[] = [],
-): void => {
+): Promise<void> => {
+  if (!turnkeySecrets) {
+    showText(
+      `Fetching Turnkey secrets from AWS Secrets Manager: ${secretName}`,
+    );
+    turnkeySecrets = await fetchSecretFromAWS(secretName);
+  }
+
   const requiredEnvs = [
     'TURNKEY_ORG_ID',
-    'TURNKEY_API_PRIVATE_KEY',
-    'TURNKEY_API_PUBLIC_KEY',
+    'TURNKEY_API_PUBLIC_KEY_SETUP',
+    'TURNKEY_API_PRIVATE_KEY_SETUP',
+    'TURNKEY_API_PUBLIC_KEY_SIGNER',
+    'TURNKEY_API_PRIVATE_KEY_SIGNER',
     ...otherEnvs,
   ];
 
-  console.log('Validating turnkey environment variables...');
-
-  const missingEnvs = requiredEnvs.filter((env) => !process.env[env]);
+  const missingEnvs = requiredEnvs.filter((env) => !turnkeySecrets![env]);
 
   if (missingEnvs.length > 0) {
     throw new Error(
-      `Missing required environment variables: ${missingEnvs.join(', ')}`,
+      `Missing required Turnkey environment variables: ${missingEnvs.join(', ')}`,
     );
   }
 };
-
-let turnkeyClientInstance: TurnkeyApiClient | null = null;
 
 /**
  * Returns a singleton instance of TurnkeyApiClient.
  * Ensures only one instance is created and reused.
  */
-export const getTurnkeyClient = (): TurnkeyApiClient => {
-  if (!turnkeyClientInstance) {
-    validateTurnkeyEnvironmentVariables();
+export const getTurnkeySignerClient = (() => {
+  let turnkeyClientInstance: TurnkeyApiClient | null = null;
 
-    // Create a new TurnkeyClient instance
-    const turnkey = new TurnkeySDKServer({
-      apiBaseUrl: 'https://api.turnkey.com',
-      apiPublicKey: process.env.TURNKEY_API_PUBLIC_KEY!,
-      apiPrivateKey: process.env.TURNKEY_API_PRIVATE_KEY!,
-      defaultOrganizationId: process.env.TURNKEY_ORG_ID!,
-    });
+  return async (
+    orgId?: string,
+    refresh?: boolean,
+  ): Promise<TurnkeyApiClient> => {
+    if (
+      !turnkeyClientInstance ||
+      refresh ||
+      (orgId && turnkeyClientInstance.config.organizationId !== orgId)
+    ) {
+      await validateTurnkeyEnvironmentVariables(TURNKEY_SECRET_NAME);
 
-    turnkeyClientInstance = turnkey.apiClient();
-  }
+      // Create a new TurnkeyClient instance
+      const turnkey = new TurnkeySDKServer({
+        apiBaseUrl: TURNKEY_BASE_URL,
+        apiPublicKey: turnkeySecrets!.TURNKEY_API_PUBLIC_KEY_SIGNER!,
+        apiPrivateKey: turnkeySecrets!.TURNKEY_API_PRIVATE_KEY_SIGNER!,
+        defaultOrganizationId: orgId ?? turnkeySecrets!.TURNKEY_ORG_ID!,
+      });
 
-  return turnkeyClientInstance;
-};
+      turnkeyClientInstance = turnkey.apiClient();
+    }
 
-const createNewWallet = async (
+    return turnkeyClientInstance;
+  };
+})();
+
+export const getTurnkeySetupClient = (() => {
+  let turnkeySetupClientInstance: TurnkeyApiClient | null = null;
+  return async (refresh?: boolean): Promise<TurnkeyApiClient> => {
+    if (!turnkeySetupClientInstance || refresh) {
+      await validateTurnkeyEnvironmentVariables(TURNKEY_SECRET_NAME);
+
+      // Create a new TurnkeyClient instance
+      const turnkeySetup = new TurnkeySDKServer({
+        apiBaseUrl: TURNKEY_BASE_URL,
+        apiPublicKey: turnkeySecrets!.TURNKEY_API_PUBLIC_KEY_SETUP!,
+        apiPrivateKey: turnkeySecrets!.TURNKEY_API_PRIVATE_KEY_SETUP!,
+        defaultOrganizationId: turnkeySecrets!.TURNKEY_ORG_ID!,
+      });
+      turnkeySetupClientInstance = turnkeySetup.apiClient();
+    }
+    return turnkeySetupClientInstance;
+  };
+})();
+
+export const createNewWallet = async (
   walletName: string,
 ): Promise<{ walletId: string; addresses: string[] }> => {
   try {
-    const turnkeyClient = getTurnkeyClient();
+    const turnkeyClient = await getTurnkeySignerClient();
 
     const { walletId, addresses } = await turnkeyClient.createWallet({
       walletName,
@@ -100,7 +114,7 @@ const createNewWallet = async (
         {
           curve: 'CURVE_SECP256K1',
           pathFormat: 'PATH_FORMAT_BIP32',
-          path: "m/44'/60'/0'/0/0", // eslint-disable-line quotes
+          path: ETHEREUM_WALLET_DEFAULT_PATH,
           addressFormat: 'ADDRESS_FORMAT_ETHEREUM',
         },
       ],
@@ -116,11 +130,14 @@ const createNewWallet = async (
   }
 };
 
-export const getSignerAccount = async (signer: Hex): Promise<Account> => {
-  const turnkeyClient = getTurnkeyClient();
+export const getSignerAccount = async (
+  signer: Hex,
+  orgId: string,
+): Promise<Account> => {
+  const turnkeyClient = await getTurnkeySignerClient(orgId);
   const account = await createAccount({
     client: turnkeyClient,
-    organizationId: process.env.TURNKEY_ORG_ID!,
+    organizationId: turnkeyClient.config.organizationId,
     signWith: signer,
   });
 
@@ -146,7 +163,10 @@ export const getProjectSigner = async (
     }
 
     // Verify the walletId and address with the Turnkey API
-    const turnkeyClient = getTurnkeyClient();
+    const turnkeyClient = await getTurnkeySignerClient(
+      walletData.subOrgId,
+      true,
+    );
 
     const {
       account: { address, walletId },
@@ -161,7 +181,7 @@ export const getProjectSigner = async (
 
     // Return the signer/address
     return {
-      signer: await getSignerAccount(address),
+      signer: await getSignerAccount(address, walletData.subOrgId),
     };
   } catch (error: any) {
     throw new Error(`Failed to fetch signer: ${error.message}`);
@@ -179,7 +199,7 @@ export const createProjectSigner = async (
 
   // create a new wallet
   try {
-    const { walletId, addresses } = await createNewWallet(
+    const { walletId, addresses, subOrgId } = await createSecureWallet(
       walletName || projectName,
     );
 
@@ -189,13 +209,12 @@ export const createProjectSigner = async (
 
     const signer = addresses[0] as Hex;
 
-    walletStore.data = { walletId, signer };
+    walletStore.data = { walletId, signer, subOrgId };
     walletStore.write();
 
-    console.log(`New wallet created and saved to ${walletStore.root}`);
-    const account = await getSignerAccount(signer);
+    showText(`New wallet created and saved to ${walletStore.root}`);
+    const account = await getSignerAccount(signer, subOrgId);
 
-    // Return the signer/address
     return {
       signer: account,
       walletStore,
@@ -204,3 +223,87 @@ export const createProjectSigner = async (
     throw new Error(`Failed to create a new wallet: ${error.message}`);
   }
 };
+
+export async function createSecureWallet(
+  collectionName: string,
+): Promise<{ subOrgId: string; walletId: string; addresses: string[] }> {
+  try {
+    const setupClient = await getTurnkeySetupClient();
+
+    // Create sub-org
+    const subOrg = await setupClient.createSubOrganization({
+      subOrganizationName: `${collectionName}-sub-org`,
+      rootQuorumThreshold: 1,
+      rootUsers: [
+        {
+          authenticators: [],
+          oauthProviders: [],
+          userName: 'setup-api-user',
+          apiKeys: [
+            {
+              apiKeyName: 'setup-api-key',
+              publicKey: turnkeySecrets!.TURNKEY_API_PUBLIC_KEY_SETUP!,
+              curveType: 'API_KEY_CURVE_P256',
+            },
+          ],
+        },
+        {
+          authenticators: [],
+          oauthProviders: [],
+          userName: 'signer-api-user',
+          apiKeys: [
+            {
+              apiKeyName: 'signer-api-key',
+              publicKey: turnkeySecrets!.TURNKEY_API_PUBLIC_KEY_SIGNER!,
+              curveType: 'API_KEY_CURVE_P256',
+            },
+          ],
+        },
+      ],
+      wallet: {
+        walletName: `${collectionName}-wallet`,
+        accounts: [
+          {
+            curve: 'CURVE_SECP256K1',
+            pathFormat: 'PATH_FORMAT_BIP32',
+            path: ETHEREUM_WALLET_DEFAULT_PATH,
+            addressFormat: 'ADDRESS_FORMAT_ETHEREUM',
+          },
+        ],
+      },
+    });
+
+    if (!subOrg) {
+      throw new Error('Failed to create sub-organization.');
+    }
+
+    const [setupUserId, signerUserId] = subOrg.rootUserIds!;
+    const wallet = subOrg.wallet!;
+
+    // Attach a signer policy to signer
+    await setupClient.createPolicy({
+      organizationId: subOrg.subOrganizationId,
+      policyName: `Allow signer to sign transactions with ${collectionName}-wallet`,
+      effect: 'EFFECT_ALLOW',
+      consensus: `approvers.any(user, user.id == '${signerUserId!}')`,
+      condition: `activity.type == 'ACTIVITY_TYPE_SIGN_TRANSACTION_V2' && wallet.id == '${wallet.walletId}'`,
+      notes: `Allow signer: ${signerUserId!} to sign transactions with wallet: ${wallet.walletId}`,
+    });
+
+    // Remove signer from root quorum
+    setupClient.updateRootQuorum({
+      organizationId: subOrg.subOrganizationId,
+      threshold: 1,
+      userIds: [setupUserId!],
+    });
+
+    showText(`✅ Wallet for ${collectionName} created and saved securely.`);
+    return {
+      walletId: wallet.walletId,
+      addresses: wallet.addresses,
+      subOrgId: subOrg.subOrganizationId,
+    };
+  } catch (error: any) {
+    throw new Error(`Failed to create a new wallet: ${error.message}`);
+  }
+}
